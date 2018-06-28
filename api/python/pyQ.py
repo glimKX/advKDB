31b9452d550e363adbfb62fcc7d33559085799bd
glimKx <glim@kx.com>
2018-06-28 14:09:31 +0000

    python to c class created

--- api/python/pyQ.py
+++ api/python/pyQ.py
@@ -2,9 +2,17 @@ import ctypes
 _qpy = ctypes.CDLL('/home/limguanyu_08/advKDB/api/python/cpython.so')
 _qpy.handle.argtypes = (ctypes.POINTER(ctypes.c_char),ctypes.c_int)
 
-def handle(host,port):
+class kdbQ:
     global qpy
-    result = _qpy.handle(ctypes.c_char_p(host),ctypes.c_int(port))
-    return int(result)
+
+    def __init__(self,host,port):
+        print("Opening Handle to q process {}:{}".format(host,port))
+        self.host=host
+        self.port=port
+        self.h=self.handle()
+
+    def handle(self):
+        result = _qpy.handle(ctypes.c_char_p(self.host),ctypes.c_int(self.port))
+        return int(result)
 
 
